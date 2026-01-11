4fab9ae60126396c16bc9a9dc9945123f3bffee7
Arthur Gasparyan <art.gasparyan.420@gmail.com>
2026-01-12 00:23:20 +0400

    fixed array for deleted photos

--- src/available-cars/dto/update-available-car.dto.ts
+++ src/available-cars/dto/update-available-car.dto.ts
@@ -1,15 +1,26 @@
 import { PartialType, ApiPropertyOptional } from '@nestjs/swagger';
 import { IsArray, IsOptional, IsString } from 'class-validator';
+import { Transform } from 'class-transformer';
 
 import { CreateAvailableCarDto } from './create-available-car.dto';
 
 export class UpdateAvailableCarDto extends PartialType(CreateAvailableCarDto) {
   @ApiPropertyOptional({
-    description: 'Array of photo URLs to delete',
-    example: ['https://example.com/photo1.jpg', 'https://example.com/photo2.jpg'],
+    description:
+      'Array of photo URLs to delete (can be a single string or array of strings)',
+    example: [
+      'https://example.com/photo1.jpg',
+      'https://example.com/photo2.jpg',
+    ],
     type: [String],
   })
   @IsOptional()
+  @Transform(({ value }) => {
+    if (typeof value === 'string') {
+      return [value];
+    }
+    return value;
+  })
   @IsArray()
   @IsString({ each: true })
   deletePhotoUrls?: string[];
--- src/vehicles/dto/update-vehicle.dto.ts
+++ src/vehicles/dto/update-vehicle.dto.ts
@@ -1,10 +1,13 @@
 import { ApiPropertyOptional, PartialType } from '@nestjs/swagger';
-import { CreateVehicleDto } from './create-vehicle.dto';
 import { IsArray, IsOptional, IsString } from 'class-validator';
+import { Transform } from 'class-transformer';
+
+import { CreateVehicleDto } from './create-vehicle.dto';
 
 export class UpdateVehicleDto extends PartialType(CreateVehicleDto) {
   @ApiPropertyOptional({
-    description: 'Array of photo URLs to delete',
+    description:
+      'Array of photo URLs to delete (can be a single string or array of strings)',
     example: [
       'https://example.com/photo1.jpg',
       'https://example.com/photo2.jpg',
@@ -12,6 +15,12 @@ export class UpdateVehicleDto extends PartialType(CreateVehicleDto) {
     type: [String],
   })
   @IsOptional()
+  @Transform(({ value }) => {
+    if (typeof value === 'string') {
+      return [value];
+    }
+    return value;
+  })
   @IsArray()
   @IsString({ each: true })
   deletePhotoUrls?: string[];
